740fc4b5ddc3ee5cfc41df0eba1af928308e0e48
jenny <jennymusah99@gmail.com>
2024-12-17 22:16:24 +0100

    first commit

---
+++ src/main/java/com/spotflow/regionaltaxcalculator/RegionalTaxCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.spotflow.regionaltaxcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegionalTaxCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegionalTaxCalculatorApplication.class, args);
+    }
+
+}
